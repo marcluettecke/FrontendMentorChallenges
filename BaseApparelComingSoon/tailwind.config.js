13374079d2384cc5a08b5773684943654c4bdd16
marcluettecke <marc.luettecke1@gmail.com>
2021-05-18 14:49:24 +0200

    finished design for challenge

--- BaseApparelComingSoon/tailwind.config.js
+++ BaseApparelComingSoon/tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		letterSpacing: {
+			tightest: '-.075em',
+			tighter: '-.05em',
+			tight: '-.025em',
+			normal: '0',
+			wider: '.05em',
+			widest: '.3em'
+		}
+	},
+
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
